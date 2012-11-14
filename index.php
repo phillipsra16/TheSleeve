58878615bc36876db93d92d81211cbdcf40475ba
pacefalm <wyattpj@email.appstate.edu>
2012-11-14 17:30:39 -0500

    Added routing variables

--- index.php
+++ index.php
@@ -24,10 +24,14 @@ $app->get ('/blog', function () {
 }); 
 
 //  /beer route. Returns beers by name
-$app->get ('/beer', function () {
-    $query = "SELECT * FROM Beer ORDER BY Name desc";
+$app->get ('/beer/{season}/{order}', function ($season, $order) {
+    // I apologize for doing this procedurally. I am a bad person because of
+    // this. I saw doing an ORM as more of a hassle than anything.
+    $query = "SELECT * FROM Beer ";
+    $where_clause = "WHERE Season = '".$season." AND ";
+    $order_clause = "ORDER BY Name ".$order;
     $sql = getConnection();
-    $beer_list = $sql->query($query);
+    $beer_list = $sql->query($query.$where_clause.$order_clause);
     $beer_array = $beer_list->fetchAll(PDO::FETCH_OBJ);
     $sql = null;
     return json_encode($beer_array);
