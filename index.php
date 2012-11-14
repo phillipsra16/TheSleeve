0be5f79f801ffdf7767d9ec0953137ba4934bf02
pacefalm <wyattpj@email.appstate.edu>
2012-11-14 16:54:10 -0500

    Added routing for blog

--- index.php
+++ index.php
@@ -12,10 +12,25 @@ $app['debug'] = true;
 // routes
 
 $app->get ('/blog', function () {
-    return "Hello World";
+    $query = "SELECT * FROM Beer ORDER BY Name desc";
+    $sql = getConnection();
+    $beer_list = $sql->query($query);
+    $beer_array = $beer_list->fetchAll(PDO::FETCH_OBJ);
+    $sql = null;
+    return json_encode($beer_array);
 }); 
 
+// Authorization
 
+function getConnection() {
+    $dbhost = "localhost";
+    $dbuser = "wyattpj";
+    $dbpass = "900458270";
+    $dbname = "3440-Tarrasque";
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
 
 $app->run();
 ?>
